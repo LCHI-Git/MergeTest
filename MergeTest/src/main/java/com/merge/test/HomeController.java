7ddc896108c071f009c749523f831c92fee74d4d
ch.lee <ch.lee>
2020-10-13 12:40:10 +0900

    dev

--- MergeTest/src/main/java/com/merge/test/HomeController.java
+++ MergeTest/src/main/java/com/merge/test/HomeController.java
@@ -38,6 +38,7 @@ public class HomeController {
 		model.addAttribute("tete", "yeah");
 		model.addAttribute("habin", "yeah");
 		model.addAttribute("habin", "yeah");
+		model.addAttribute("this", "dev");
 		
 		return "home";
 	}
